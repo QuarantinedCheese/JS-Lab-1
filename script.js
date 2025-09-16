7aee3bab40b80024aee92b1ab12a238cda227d14
QuarantinedCheese <ethanpothier2@gmail.com>
2025-09-16 15:46:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Q1 Declare and Use Variables
+var studentName = "Jason"
+var studentAge = 16
+var isEnrolled = true
+
+console.log(studentName)
+console.log(studentAge)
+console.log(isEnrolled)
+
+// Also Q1 because they're marked wrong... so I'll call Q2 Demonstrate Data Types
+var string = "This is a string!"
+console.log(string)
+
+var number = 419
+console.log(number)
+
+var truefalse = true
+console.log(truefalse)
+
+var coolarray = ["Apple", "Biking", "Falled down :("]
+console.log(coolarray)
+
+// Q3 Basic Operators
+console.log(4+4)
+console.log(4-4)
+console.log(4*4)
+console.log(4/4)
+
+console.log(10 > 3)
+console.log(10 < 11)
+console.log(5 == 3)
+console.log(9 != "Hello!")
+
+console.log((3==3) && ("France" != "Cool"))
+console.log(!(3==3))
+console.log(("Hello" == "Goodbye") || studentAge == 16)
+
